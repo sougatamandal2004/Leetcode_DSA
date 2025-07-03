e7a4898d424913cb2ed0a56a99d4e9f53557c474
Sougata Mandal <158832491+sougatamandal2004@users.noreply.github.com>
2025-07-03 17:22:14 +0530

    Time: 1 ms (99.58%), Space: 56.9 MB (81.37%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,5 +1,19 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+
+        // O(n^2) time complexity
+        // int n = nums.length;
+        // int maxSum = Integer.MIN_VALUE;
+        // for(int i=0;i<n;i++){
+        //     int currSum = 0;
+        //     for(int j=i;j<n;j++){
+        //         currSum += nums[j];
+        //         maxSum = Math.max(currSum, maxSum);
+        //     }
+        // }
+        // return maxSum; // this is not workable in here cause it give time limit exceed for large size of array
+
+
         int maxSum = nums[0];
         int currSum = nums[0];
         for(int i=1;i<nums.length;i++){
