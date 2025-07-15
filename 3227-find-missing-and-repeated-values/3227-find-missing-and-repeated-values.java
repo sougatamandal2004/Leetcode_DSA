265c5b851d6cb91e1d5b8f4699f7a0860e0a0320
Sougata Mandal <158832491+sougatamandal2004@users.noreply.github.com>
2025-07-15 13:09:32 +0530

    Time: 1 ms (100%), Space: 45.6 MB (42.42%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -1,23 +1,44 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int[] arr = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int r = grid.length;
-        int c = grid[0].length;
-        int number = r*c;
-        int val = number*(number+1)/2;
-        for(int i=0;i<r;i++){
+        // int[] arr = new int[2];
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // int r = grid.length;
+        // int c = grid[0].length;
+        // int number = r*c;
+        // int val = number*(number+1)/2;
+        // for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //         map.put(grid[i][j], map.getOrDefault(grid[i][j], 0) + 1);
+        //         if(map.get(grid[i][j]) == 2){
+        //             arr[0] = grid[i][j];
+        //         }
+        //         else{
+        //             val -= grid[i][j];
+        //         }
+        //     }
+        // }
+        // arr[1] = val;
+        // return arr;
+
+
+        // another way
+         int r = grid.length;
+         int c = grid[0].length;
+         int[] count =  new int[r*c+1];
+         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                map.put(grid[i][j], map.getOrDefault(grid[i][j], 0) + 1);
-                if(map.get(grid[i][j]) == 2){
-                    arr[0] = grid[i][j];
-                }
-                else{
-                    val -= grid[i][j];
-                }
+                count[grid[i][j]]++;
             }
-        }
-        arr[1] = val;
-        return arr;
+         }
+         int rep = -1, miss = -1;
+         for(int i=1;i<=r*c;i++){
+            if(count[i] == 2){
+                rep = i;
+            }
+            if(count[i] == 0){
+                miss = i;
+            }
+         }
+         return new int[]{rep, miss};
     }
 }
