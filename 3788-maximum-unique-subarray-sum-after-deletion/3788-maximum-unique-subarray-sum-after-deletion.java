fa20b7644fad36ac48f94b620d5b09949f5bdd31
Sougata Mandal <158832491+sougatamandal2004@users.noreply.github.com>
2025-07-25 17:47:06 +0530

    Time: 1 ms (100%), Space: 42.3 MB (78.44%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -1,18 +1,34 @@
 class Solution {
     public int maxSum(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int num : nums){
-            set.add(num);
-        }
-        int output = 0;
-        for(int num : set){
-            if(num > 0){
-                output += num;
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int num : nums){
+        //     set.add(num);
+        // }
+        // int output = 0;
+        // for(int num : set){
+        //     if(num > 0){
+        //         output += num;
+        //     }
+        // }
+        // if(output <= 0) {
+        //     output = Collections.max(set);
+        // }
+        // return output;
+
+
+        // best approach
+        Arrays.sort(nums);
+        int prev = nums[nums.length - 1];
+        int sum = prev;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int n = nums[i];
+            if (n <= 0) {
+                return sum;
+            } else if (n != prev) {
+                sum = sum + n;
             }
+            prev = n;
         }
-        if(output <= 0) {
-            output = Collections.max(set);
-        }
-        return output;
+        return sum;
     }
 }
