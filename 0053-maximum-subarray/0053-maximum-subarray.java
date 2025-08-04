e306e4c2a37526f428001e7fcd0f3514cde2a94a
Sougata Mandal <158832491+sougatamandal2004@users.noreply.github.com>
2025-08-04 14:56:12 +0530

    Time: 1 ms (99.53%), Space: 57.3 MB (31.1%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,33 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //brute force
-        // int maxSum = Integer.MIN_VALUE;
-        // int len = nums.length;
-        // for(int i=0;i<len;i++){
-        //     int s = i;
-        //     for(int j=i;j<len;j++){
-        //         int e = j;
-        //         int currSum = 0;
-        //         for(int k=s;k<=e;k++){
-        //             currSum += nums[k];
-        //         }
-        //         if(currSum > maxSum){
-        //             maxSum = currSum;
-        //         }
-        //     }
-        // }
-        // return maxSum;
-
-
-        // Optimal -> Kadane's algorithm
+        int currSum = nums[0];
         int maxSum = nums[0];
-        int currSum = 0;
-        for(int i=0;i<nums.length;i++){
-            currSum += nums[i];
-            maxSum = Math.max(currSum, maxSum);
-            if(currSum < 0) {
-                currSum = 0;
-            }
+        for(int i=1;i<nums.length;i++){
+            currSum = Math.max(currSum + nums[i], nums[i]);
+            // if(currSum <= 0){
+            //     currSum = 0;
+            // }
+            maxSum = Math.max(maxSum, currSum);
         }
         return maxSum;
     }
