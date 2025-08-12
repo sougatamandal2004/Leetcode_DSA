b009906b5c7ad2449fe456ab7a383dca26f4f31f
Sougata Mandal <158832491+sougatamandal2004@users.noreply.github.com>
2025-08-12 18:54:52 +0530

    Time: 2 ms (74.03%), Space: 53.1 MB (49.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -15,38 +15,61 @@ class Solution {
         // return majorEle;
 
 
-        // best approach without extra space and tc is O(n log n)
-        return majority(nums, 0, nums.length - 1);
-    }
-    private static int majority(int[] nums, int si, int ei) {
-        if(si == ei) {
-            return nums[si];
+
+
+        // better approach without extra space and tc is O(n log n)
+        // return majority(nums, 0, nums.length - 1);
+
+
+
+
+        
+    // this is the best approach
+    // Boyer-Moore Voting Algorithm
+
+    Integer candidate = null;
+    int count = 0;
+    for(int num : nums) {
+        if(count == 0) {
+            candidate = num;
+            count++;
         }
-        // finding mid
-        int mid = si + (ei - si) / 2;
-        int left = majority(nums, si, mid);
-        int right = majority(nums, mid + 1, ei);
-
-        // checking both are equal or not
-        if(left == right) {
-            return left;
+        else if(candidate != num) {
+            count--;
         }
-
-        // checking whose count is greater
-        int leftCount = count(nums, left, si, ei);
-        int rightCount = count(nums, right, si, ei);
-
-        // returning value
-        return (leftCount > rightCount) ? left : right;
+        else count++;
     }
-
-    private static int count(int[] nums, int num, int si, int ei) {
-        int count = 0;
-        for(int i=si;i<=ei;i++){
-            if(nums[i] == num) {
-                count++;
-            }
-        }
-        return count;
+    return candidate;
     }
+    // private static int majority(int[] nums, int si, int ei) {
+    //     if(si == ei) {
+    //         return nums[si];
+    //     }
+    //     // finding mid
+    //     int mid = si + (ei - si) / 2;
+    //     int left = majority(nums, si, mid);
+    //     int right = majority(nums, mid + 1, ei);
+
+    //     // checking both are equal or not
+    //     if(left == right) {
+    //         return left;
+    //     }
+
+    //     // checking whose count is greater
+    //     int leftCount = count(nums, left, si, ei);
+    //     int rightCount = count(nums, right, si, ei);
+
+    //     // returning value
+    //     return (leftCount > rightCount) ? left : right;
+    // }
+
+    // private static int count(int[] nums, int num, int si, int ei) {
+    //     int count = 0;
+    //     for(int i=si;i<=ei;i++){
+    //         if(nums[i] == num) {
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
 }
