a61ca77c00dea2bb028c157e2f1bd29b45f2a4c8
Sougata Mandal <158832491+sougatamandal2004@users.noreply.github.com>
2025-08-12 18:46:00 +0530

    Time: 1 ms (99.87%), Space: 53.3 MB (29.09%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,52 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int num : nums){
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
+        // int majorEle = -1;
+        // int count = 0;
+        // for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        //     if(entry.getValue() > count){
+        //         count = entry.getValue();
+        //         majorEle = entry.getKey();
+        //     }
+        // }
+        // return majorEle;
+
+
+        // best approach without extra space and tc is O(n log n)
+        return majority(nums, 0, nums.length - 1);
+    }
+    private static int majority(int[] nums, int si, int ei) {
+        if(si == ei) {
+            return nums[si];
         }
-        int majorEle = -1;
+        // finding mid
+        int mid = si + (ei - si) / 2;
+        int left = majority(nums, si, mid);
+        int right = majority(nums, mid + 1, ei);
+
+        // checking both are equal or not
+        if(left == right) {
+            return left;
+        }
+
+        // checking whose count is greater
+        int leftCount = count(nums, left, si, ei);
+        int rightCount = count(nums, right, si, ei);
+
+        // returning value
+        return (leftCount > rightCount) ? left : right;
+    }
+
+    private static int count(int[] nums, int num, int si, int ei) {
         int count = 0;
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if(entry.getValue() > count){
-                count = entry.getValue();
-                majorEle = entry.getKey();
+        for(int i=si;i<=ei;i++){
+            if(nums[i] == num) {
+                count++;
             }
         }
-        return majorEle;
+        return count;
     }
 }
