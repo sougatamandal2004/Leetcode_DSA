e7b6018756d497b68677aec8a0963d45b3a068e8
Sougata Mandal <158832491+sougatamandal2004@users.noreply.github.com>
2025-08-21 07:58:40 +0530

    Time: 12 ms (99.91%), Space: 64.6 MB (67.65%) - LeetHub

--- 2270-find-all-lonely-numbers-in-the-array/2270-find-all-lonely-numbers-in-the-array.java
+++ 2270-find-all-lonely-numbers-in-the-array/2270-find-all-lonely-numbers-in-the-array.java
@@ -1,25 +1,44 @@
 class Solution {
     public List<Integer> findLonely(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        // brute force => using double for loop => TC = O(n^2)
-        Arrays.sort(nums);
-        int len = nums.length;
-        if(len == 0) return list;
-        if(len == 1) {
-            list.add(nums[0]);
-            return list;
+        // List<Integer> list = new ArrayList<>();
+        // // brute force => using double for loop => TC = O(n^2)
+        // Arrays.sort(nums);
+        // int len = nums.length;
+        // if(len == 0) return list;
+        // if(len == 1) {
+        //     list.add(nums[0]);
+        //     return list;
+        //     }
+        // for(int i=1;i<nums.length-1;i++) {
+        //     if((nums[i-1] + 1 < nums[i]) && (nums[i] + 1 < nums[i+1])){
+        //         list.add(nums[i]);
+        //     }
+        // }
+        // if(nums[0] + 1 < nums[1]){
+        //     list.add(nums[0]);
+        // }
+        // if(nums[len - 2] + 1 < nums[len - 1]){
+        //     list.add(nums[len-1]);
+        // }
+        // return list;
+
+
+        int max = nums[0];
+        for(int num: nums){
+            if(num > max){
+                max = num;
             }
-        for(int i=1;i<nums.length-1;i++) {
-            if((nums[i-1] + 1 < nums[i]) && (nums[i] + 1 < nums[i+1])){
-                list.add(nums[i]);
+        }
+        int[] map = new int[max+2];
+        for (int num : nums) {
+            map[num]++;
+        }
+        List<Integer> res = new ArrayList<>();
+		for (int num : nums) {
+            if (map[num] == 1 && (num==0 || map[num-1]==0) && map[num+1]==0) {
+                res.add(num);
             }
         }
-        if(nums[0] + 1 < nums[1]){
-            list.add(nums[0]);
-        }
-        if(nums[len - 2] + 1 < nums[len - 1]){
-            list.add(nums[len-1]);
-        }
-        return list;
+        return res;
     }
 }
