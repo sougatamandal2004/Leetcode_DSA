8a60b4c0acd3b8ddbce8296bdfcb217e14eaaad9
Sougata Mandal <158832491+sougatamandal2004@users.noreply.github.com>
2025-09-12 08:52:14 +0530

    Time: 272 ms (6%), Space: 58.9 MB (55.61%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n-k+1];
+        List<Integer> list = new ArrayList<>();
+        int i=0,j=0;
+        while(j < n){
+            while(!list.isEmpty() && list.get(list.size() - 1) < nums[j]){
+                list.remove(list.size() - 1);
+            }
+            list.add(nums[j]);
+            if(j - i + 1 < k){
+                j++;
+            }
+            else if(j - i + 1 == k) {
+                int x = list.get(0);
+                ans[i] = x;
+                if(x == nums[i]){
+                    list.remove(0);
+                }
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+}
