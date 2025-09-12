9800f5be445cd9c4f3bb9b37847c0419a9365611
Sougata Mandal <158832491+sougatamandal2004@users.noreply.github.com>
2025-09-12 22:01:19 +0530

    Time: 6 ms (42.34%), Space: 43.6 MB (61.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        // if(s.length() != t.length()) return false;
+        // int[] count = new int[26];
+        // for(int i=0;i<t.length();i++){
+        //     char ch = t.charAt(i);
+        //     int idx = s.indexOf(ch);
+        //     if(idx != -1){
+        //         s = s.substring(0,idx) + s.substring(idx + 1);
+        //     }
+        // }
+        // return s.length() == 0;
+
+
+        // optimal approach
         if(s.length() != t.length()) return false;
         int[] count = new int[26];
         for(int i=0;i<t.length();i++){
