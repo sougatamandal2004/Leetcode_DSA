5b732cdcc0e5be2ceab72787fe7aef0c74400dfd
Sougata Mandal <158832491+sougatamandal2004@users.noreply.github.com>
2025-09-12 21:29:00 +0530

    Time: 4 ms (61.71%), Space: 41.9 MB (98.44%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -1,19 +1,41 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        Stack<Character> st = new Stack<>();
+        // Stack<Character> st = new Stack<>();
+        // StringBuilder sb = new StringBuilder();
+        // for(int i=0;i<s.length();i++){
+        //     char c = s.charAt(i);
+        //     if(c == '('){
+        //        if(!st.isEmpty()){
+        //             sb.append(c);
+        //        }
+        //        st.push(c);
+        //     }
+        //     else{
+        //         st.pop();
+        //         if(!st.isEmpty()){
+        //             sb.append(c);
+        //         }
+        //     }
+        // }
+        // return sb.toString();
+
+
+
+        // best solution
         StringBuilder sb = new StringBuilder();
+        int depth = 0;
         for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(c == '('){
-               if(!st.isEmpty()){
-                    sb.append(c);
-               }
-               st.push(c);
+            char ch = s.charAt(i);
+            if(ch == '('){
+                if(depth > 0){
+                    sb.append(ch);
+                }
+                depth++;
             }
             else{
-                st.pop();
-                if(!st.isEmpty()){
-                    sb.append(c);
+                depth--;
+                if(depth > 0){
+                    sb.append(ch);
                 }
             }
         }
