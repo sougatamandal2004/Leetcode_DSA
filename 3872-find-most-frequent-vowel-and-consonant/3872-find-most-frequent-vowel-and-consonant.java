7d48063726eac76dfa160a8a4010b5cf0e83f872
Sougata Mandal <158832491+sougatamandal2004@users.noreply.github.com>
2025-09-13 10:19:47 +0530

    Time: 1 ms (100%), Space: 42.2 MB (91.95%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -1,27 +1,44 @@
 class Solution {
     public int maxFreqSum(String s) {
-        Map<Character, Integer> map = new HashMap<>();
+        // Map<Character, Integer> map = new HashMap<>();
+        // for(char ch : s.toCharArray()){
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // }
+        // int x = 0, y = 0;
+        // for(Map.Entry<Character, Integer> entry : map.entrySet()){
+        //     char key = entry.getKey();
+        //     int value = entry.getValue();
+        //     if(isVowel(key)){
+        //         if(value > x){
+        //             x = value;
+        //         }
+        //     }
+        //     else{
+        //         if(value > y){
+        //             y = value;
+        //         }
+        //     }
+        // }
+        // return (x + y);
+
+
+        // another way
+        int[] count = new int[26];
         for(char ch : s.toCharArray()){
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            count[ch - 'a']++;
         }
         int x = 0, y = 0;
-        for(Map.Entry<Character, Integer> entry : map.entrySet()){
-            char key = entry.getKey();
-            int value = entry.getValue();
-            if(isVowel(key)){
-                if(value > x){
-                    x = value;
-                }
+        for(int i = 0; i< 26;i++){
+            if(i == 0 || i == 4 || i == 8 || i == 14 || i == 20){
+                if(count[i] > x) x = count[i];
             }
             else{
-                if(value > y){
-                    y = value;
-                }
+                if(count[i] > y) y = count[i];
             }
         }
         return (x + y);
     }
-    private boolean isVowel(char ch){
-        return "aeiou".indexOf(ch) != -1;
-    }
+    // private boolean isVowel(char ch){
+    //     return "aeiou".indexOf(ch) != -1;
+    // }
 }
