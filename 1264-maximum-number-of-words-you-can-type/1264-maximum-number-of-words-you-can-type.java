ba71c9108d4f58b81cacf986c2c38b254c7531d7
Sougata Mandal <158832491+sougatamandal2004@users.noreply.github.com>
2025-09-15 11:51:41 +0530

    Time: 3 ms (48.72%), Space: 41.8 MB (98.38%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
@@ -1,15 +1,36 @@
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
-        String[] arr = text.split("\\s+");
-        int ans = arr.length;
-        for(String s : arr){
-            for(char ch : brokenLetters.toCharArray()){
-                if(s.indexOf(ch) != -1){
-                    ans -= 1;
-                    break;
-                }
+        // String[] arr = text.split("\\s+");
+        // int ans = arr.length;
+        // for(String s : arr){
+        //     for(char ch : brokenLetters.toCharArray()){
+        //         if(s.indexOf(ch) != -1){
+        //             ans -= 1;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        // optimal approach
+        boolean[] broken = new boolean[26];
+        for(char ch : brokenLetters.toCharArray()){
+            broken[ch - 'a'] = true;
+        }
+
+        int ans = 0;
+        boolean valid = true;
+        for(int i=0;i<text.length();i++){
+            char ch = text.charAt(i);
+            if(ch == ' '){
+                if(valid) ans++;
+                valid = true;
+            }
+            else{
+                if(broken[ch - 'a']) valid = false;
             }
         }
+        if(valid) ans++;
         return ans;
     }
 }
