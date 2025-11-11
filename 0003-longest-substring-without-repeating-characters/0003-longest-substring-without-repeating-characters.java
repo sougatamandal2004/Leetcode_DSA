815710085e9c587bcb5da4909af8c42d88649e77
sougatagithubrit <158832491+sougatamandal2004@users.noreply.github.com>
2025-11-11 09:45:42 +0530

    Time: 8 ms (18.33%), Space: 45.7 MB (11.17%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,44 +1,17 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // HashMap<Character, Integer> map = new HashMap<>();
-        // int i=0, j = 0, size = s.length();
-        // int maxLen = 0;
-        // while(j < size){
-        //     char ch = s.charAt(j);
-        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
-        //     while(map.get(ch) > 1){
-        //         char left = s.charAt(i);
-        //         map.put(left, map.get(left) - 1);
-        //         if(map.get(left) == 0) map.remove(left);
-        //         i++;
-        //     }
-        //     maxLen = Math.max(maxLen, j - i + 1);
-        //     j++;
-        // }
-        // return maxLen;
-
-
-        // another way
         HashMap<Character, Integer> map = new HashMap<>();
-        int i=0, j = 0, size = s.length();
-        int maxLen = 0;
-        int count = 0;
+        int i=0, j=0, size = s.length(), maxLen = 0;
         while(j < size){
             char ch = s.charAt(j);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
-            count++;
-            if(map.size() == count){
-                maxLen = Math.max(maxLen, j - i + 1);
+            while(map.get(ch) > 1){
+                char left = s.charAt(i);
+                map.put(left, map.get(left) - 1);
+                if(map.get(left) == 0) map.remove(left);
+                i++;
             }
-            else{
-                while(map.get(ch) > 1){
-                    char left = s.charAt(i);
-                    map.put(left, map.get(left) - 1);
-                    if(map.get(left) == 0) map.remove(left);
-                    i++;
-                    count--;
-                }
-            }
+            maxLen = Math.max(maxLen, j - i + 1);
             j++;
         }
         return maxLen;
