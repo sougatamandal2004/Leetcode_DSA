7e42d0531aa8c57898811a616485d410c53db418
sougatagithubrit <158832491+sougatamandal2004@users.noreply.github.com>
2025-11-11 12:12:03 +0530

    Time: 3 ms (100%), Space: 175.4 MB (7.92%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,23 +1,37 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int n = nums.length;
+        // int n = nums.length;
+        // int[] ans = new int[nums.length];
+        // ArrayList<Integer> list1 = new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i] >= 0){
+        //         list1.add(nums[i]);
+        //     }
+        // }
+        // ArrayList<Integer> list2 = new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i] < 0){
+        //         list2.add(nums[i]);
+        //     }
+        // }
+        // int x = 0;
+        // for(int i=0;i<list1.size();i++){
+        //     ans[x++] = list1.get(i);
+        //     ans[x++] = list2.get(i);
+        // }
+        // return ans;
+
         int[] ans = new int[nums.length];
-        ArrayList<Integer> list1 = new ArrayList<>();
+        int x = 0, y = 0;
         for(int i=0;i<nums.length;i++){
             if(nums[i] >= 0){
-                list1.add(nums[i]);
+                ans[2*x] = nums[i];
+                x++;
             }
-        }
-        ArrayList<Integer> list2 = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] < 0){
-                list2.add(nums[i]);
+            else{
+                ans[2*y+1] = nums[i];
+                y++;
             }
-        }
-        int x = 0;
-        for(int i=0;i<list1.size();i++){
-            ans[x++] = list1.get(i);
-            ans[x++] = list2.get(i);
         }
         return ans;
     }
