a8faa78edb8e5ed06199cbc271cb805dd4b9334b
sougatagithubrit <158832491+sougatamandal2004@users.noreply.github.com>
2025-11-13 06:29:45 +0530

    Time: 7 ms (78.52%), Space: 47.3 MB (24.44%) - LeetHub

---
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxOperations(String s) {
+       int ans = 0, ones = 0;
+       for(int i=0;i<s.length();i++){
+        if(s.charAt(i) == '1'){
+            ones++;
+        } else if(i == s.length()-1 || s.charAt(i+1) == '1'){
+            ans += ones;
+            }
+       }
+       return ans;
+    }
+}
