9528fd2413ca04b05dcaefc8e00116989f07d0ac
sougatagithubrit <158832491+sougatamandal2004@users.noreply.github.com>
2026-01-21 08:34:21 +0530

    Time: 1 ms (99.73%), Space: 62.3 MB (20.95%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -8,10 +8,11 @@ class Solution {
             sum += nums[j];
             if(sum >= target){
                 while(sum >= target){
+                    ans = Math.min(ans, j - i + 1); // but this one is much better than the below one cause here we don't go out extra index which is not present in the array
                     sum -= nums[i];
                     i++;
                 }
-                ans = Math.min(ans, j - i + 1+1);
+                //  ans = Math.min(ans, j - i + 1+1); // => if we take here this will also work
             }
         }
         return ans == Integer.MAX_VALUE ? 0 : ans;
