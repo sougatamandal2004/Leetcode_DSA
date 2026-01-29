9519bb9d17cad37cd8a86155944216862867e33a
sougatagithubrit <158832491+sougatamandal2004@users.noreply.github.com>
2026-01-29 18:36:14 +0530

    Time: 43 ms (26.1%), Space: 24.5 MB (12.57%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size();
-        if(n <= 2) return n;
         int result = 0;
+        if(n <= 2) return n;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int cnt = 2;
-                int dy = points[j][1] - points[i][1];
-                int dx = points[j][0] - points[i][0];
+            unordered_map<double, int> map;
+            for(int j=0;j<n;j++){
+                if(i == j) continue;
+                auto dy = points[j][1] - points[i][1];
+                auto dx = points[j][0] - points[i][0];
 
-                for(int k = 0; k<n;k++){
-                    if(k == i || k == j) continue;
-                    int dy_ = points[k][1] - points[i][1];
-                    int dx_ = points[k][0] - points[i][0];
-
-                    // calculating slope
-                    if(dy*dx_ == dy_*dx){
-                        cnt++;
-                    }
-                }
-                result = max(result, cnt);
+                auto theta = atan2(dy, dx);
+                map[theta]++;
+            }
+            for(auto& it : map){
+                result = max(result, it.second+1);
             }
         }
         return result;
