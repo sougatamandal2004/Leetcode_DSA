75e7117e6b3a94931f0263b498982ce674d2b2cd
sougatagithubrit <158832491+sougatamandal2004@users.noreply.github.com>
2026-01-30 16:44:20 +0530

    Time: 31 ms (70.24%), Space: 16.8 MB (10.16%) - LeetHub

---
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int count = 0;
+        for(int i=2;i<n;i++){
+            int l = 0, r = i-1;
+            while(l < r){ 
+                if(nums[l] + nums[r] > nums[i]){
+                    count += (r - l);
+                    r--;
+                } else{
+                    l++;
+                }
+            }
+        }
+        return count;
+    }
+};
