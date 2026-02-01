af11ea9d284fa0cb6afd307fb3bd9b5b07a2647f
sougatagithubrit <158832491+sougatamandal2004@users.noreply.github.com>
2026-02-01 11:11:32 +0530

    Time: 0 ms (100%), Space: 11.3 MB (29.97%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,10 +3,15 @@ public:
     vector<int> countBits(int n) {
         vector<int> v;
         v.push_back(0);
-        for(int i=1;i<=n;i++){
-            v.push_back(v[i/2]);
-            if(i%2) v[i] += 1;
-
+        v.push_back(1);
+        if(n == 0) return {0};
+        if(n == 1) return v;
+        for(int i=2;i<=n;i++){
+            if(i%2 == 0) {
+                v.push_back(v[i/2]);
+            } else{
+                v.push_back(v[i/2] + 1);
+            }
         }
         return v;
     }
