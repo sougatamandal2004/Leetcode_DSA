9700ea34d69acaeca2246b55287ae32031e854cf
sougatagithubrit <158832491+sougatamandal2004@users.noreply.github.com>
2026-02-07 10:56:18 +0530

    Time: 2 ms (71.07%), Space: 18.2 MB (91.06%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,42 +10,38 @@
  */
 class Solution {
 public:
+    struct cmp {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;   // min heap
+        }
+    };
+
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size() == 0) return NULL;
-        while(lists.size() > 1){
-            lists.push_back(mergeTwoLists(lists[0], lists[1]));
-            lists.erase(lists.begin());
-            lists.erase(lists.begin());
-        }
-        return lists.front();
-    }
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 == nullptr) return list2;
-        if(list2 == nullptr) return list1;
-        ListNode* curr = new ListNode(-1);
-        ListNode* temp = curr;
-        while(list1 != nullptr && list2 != nullptr){
-            if(list1 -> val < list2 -> val){
-                temp -> next = list1;
-                list1 = list1 -> next;
-                temp = temp -> next;
-            }
-            else{
-                temp -> next = list2;
-                list2 = list2 -> next;
-                temp = temp -> next;
+        priority_queue<ListNode*, vector<ListNode*>, cmp> pq;
+
+        // push first node of each list
+        for (ListNode* head : lists) {
+            if (head != nullptr) {
+                pq.push(head);
             }
         }
-        while(list1){
-            temp -> next = list1;
-            list1 = list1 -> next;
-            temp = temp -> next;
+
+        // dummy node to build answer
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+
+        while (!pq.empty()) {
+            ListNode* node = pq.top();
+            pq.pop();
+
+            tail->next = node;
+            tail = tail->next;
+
+            if (node->next != nullptr) {
+                pq.push(node->next);
+            }
         }
-        while(list2){
-            temp -> next = list2;
-            list2 = list2 -> next;
-            temp = temp -> next;
-        }
-        return curr -> next;
+
+        return dummy->next;
     }
 };
